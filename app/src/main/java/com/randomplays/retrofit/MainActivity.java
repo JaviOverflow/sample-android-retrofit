b36564eb280e067a2368d5158f2d8838f4658b80
JaviOverflow <javirs007@gmail.com>
2016-06-10 17:35:11 +0200

    2.0.2 retrofit version

---
+++ app/src/main/java/com/randomplays/retrofit/MainActivity.java
@@ -0,0 +1,75 @@
+package com.randomplays.retrofit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String API_URL = "https://api.github.com";
+
+    public static class Contributor {
+        public final String login;
+        public final int contributions;
+
+        public Contributor(String login, int contributions) {
+            this.login = login;
+            this.contributions = contributions;
+        }
+    }
+
+    public interface GitHub {
+        @GET("/repos/{owner}/{repo}/contributors")
+        Call<List<Contributor>> contributors(
+                @Path("owner") String owner,
+                @Path("repo") String repo);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create a very simple REST adapter which points the GitHub API.
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        // Create an instance of our GitHub API interface.
+        GitHub github = retrofit.create(GitHub.class);
+
+
+        // Create a call instance for looking up Retrofit contributors.
+        Call<List<Contributor>> call = github.contributors("square", "retrofit");
+
+        call.enqueue(new Callback<List<Contributor>>() {
+            @Override
+            public void onResponse(Call<List<Contributor>> call, Response<List<Contributor>> response) {
+
+                List<Contributor> contributors = response.body();
+
+                for (Contributor contributor : contributors)
+                    Log.i("me", contributor.login + " (" + contributor.contributions + ")");
+            }
+
+            @Override
+            public void onFailure(Call<List<Contributor>> call, Throwable t) {
+                Log.i("me", "Ha ido mal");
+            }
+        });
+
+    }
+
+
+}
